6693c4e208e1beade420dcdfc2fa8d3027e210a9
Brielle-Chenier <brielle_chenier@live.com>
2021-02-21 00:26:24 -0500

    all coutries (almost)

--- flaskApp/static/initMap.js
+++ flaskApp/static/initMap.js
@@ -1,37 +1,46 @@
 function initMap() {
-    const myLatlng = { lat: 26, lng: -24 };
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 3,
-      center: myLatlng,
-    });
-    // Create the initial InfoWindow.
-    let infoWindow = new google.maps.InfoWindow({
-      content: "Click the map to get Lat/Lng!",
-      position: myLatlng,
-    });
-    infoWindow.open(map);
-    // Configure the click listener.
-    map.addListener("click", (mapsMouseEvent) => {
-      // Close the current InfoWindow.
-      infoWindow.close();
+  const myLatlng = { lat: 26, lng: -24 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 3,
+    center: myLatlng,
+  });
+  // Create the initial InfoWindow.
+  let infoWindow = new google.maps.InfoWindow({
+    content: "Click the map to get Lat/Lng!",
+    position: myLatlng,
+  });
+  infoWindow.open(map);
+  // Configure the click listener.
+  map.addListener("click", (mapsMouseEvent) => {
+    // Close the current InfoWindow.
+    infoWindow.close();
 
-      // delete json stuff so it's just the coordinates
-      var str = JSON.stringify(mapsMouseEvent.latLng.toJSON());
-      var str2 = str.replace('{"lat":', '').toString();
-      var str3 = str2.replace('"lng":', '').toString();
-      var str4 = str3.replace('}', '');
+    // delete json stuff so it's just the coordinates
+    var str = JSON.stringify(mapsMouseEvent.latLng.toJSON());
+    var str2 = str.replace('{"lat":', '').toString();
+    var str3 = str2.replace('"lng":', '').toString();
+    var str4 = str3.replace('}', '');
 
-      // Link to the api, i don't know how to get the data out of this :(
-      var Link = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='+ str4.toString() +'&key=AIzaSyCy4rMsW8E0G5S7kJAH6rgXOPOl6MizZsw&language=en&result_type=country';
-
-      // Create a new InfoWindow.
-      infoWindow = new google.maps.InfoWindow({
-        position: mapsMouseEvent.latLng,
+    // Link to the api
+    var link = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='+ str4.toString() +'&key=AIzaSyCy4rMsW8E0G5S7kJAH6rgXOPOl6MizZsw&language=en&result_type=country';
+    
+    // Get info from Geocoding API
+    $(document).ready(function() {
+      $.getJSON(link, function(data) {
+        // Display country name
+        document.getElementById('test').innerHTML = data.results[0].formatted_address;
       });
-      infoWindow.setContent(
-        //JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
-        'https://maps.googleapis.com/maps/api/geocode/json?latlng='+ str4.toString() +'&key=AIzaSyCy4rMsW8E0G5S7kJAH6rgXOPOl6MizZsw&language=en&result_type=country'
-      );
-      infoWindow.open(map);
     });
-  }
+
+
+    // Create a new InfoWindow.
+    infoWindow = new google.maps.InfoWindow({
+      position: mapsMouseEvent.latLng,
+    });
+    infoWindow.setContent(
+      link
+      //'https://maps.googleapis.com/maps/api/geocode/json?latlng='+ str4.toString() +'&key=AIzaSyCy4rMsW8E0G5S7kJAH6rgXOPOl6MizZsw&language=en&result_type=country'
+    );
+    infoWindow.open(map);
+  });
+}
