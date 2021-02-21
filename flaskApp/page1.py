8568730bfac7fc0a6fc4084c1d88d993be6a6a11
Brielle-Chenier <brielle_chenier@live.com>
2021-02-20 22:58:48 -0500

    woo it works

--- flaskApp/page1.py
+++ flaskApp/page1.py
@@ -1,10 +1,15 @@
 from flask import Flask, redirect, url_for, render_template
+import csv
+from spotipy_app import add_songs_to_playlist
+import spotipy
+from Searching_For_Artists import search
 
 app = Flask (__name__)
 
 @app.route("/<country>")
 def home(country):
-    return render_template("index.html", link = country)
+    link = search(country)
+    return render_template("index.html", link = link)
 
 @app.route("/")
 def home1():
