0aea797ba744e5d6134120747822b85953cf6990
Brielle-Chenier <brielle_chenier@live.com>
2021-02-21 01:51:26 -0500

    separated string to search for artist

--- Searching_For_Artists.py
+++ Searching_For_Artists.py
@@ -2,17 +2,18 @@ import csv
 from spotipy_app import add_songs_to_playlist
 
 def search(country):
-    with open('C:\\Users\\brie\\OneDrive\\Documents\\GitHub\\Hackathons\\PearlHacks21\\artists.csv', 'r') as csv_file:
+    with open('C:\\Users\\briel\\OneDrive\\Documents\\GitHub\\Hackathons\\PearlHacks21\\artists.csv', 'r') as csv_file:
         csv_reader = csv.reader(csv_file)
 
         local_artists = []
 
         for row in csv_reader:
-            if row [0] == country:
-                print (row[1])
-                local_artists.append(row[1])
+            if country in str(row [0]):
+                new_row = str(row[0]).split(',')
+                local_artists.append(str(new_row[1]))
 
-        print (local_artists)
+        for artist in local_artists:
+            print (artist)
         #add_songs_to_playlist(local_artists)
 
 
