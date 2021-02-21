64dd7171928de2073c7e75051af4fa293c5cc18e
isabelle-abbas <79290666+isabelle-abbas@users.noreply.github.com>
2021-02-20 20:28:58 -0800

    getting the country name from the map

--- templates/initMap.js
+++ templates/initMap.js
@@ -21,16 +21,25 @@ function initMap() {
       var str3 = str2.replace('"lng":', '').toString();
       var str4 = str3.replace('}', '');
 
-      // Link to the api, i don't know how to get the data out of this :(
-      var Link = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='+ str4.toString() +'&key=AIzaSyCy4rMsW8E0G5S7kJAH6rgXOPOl6MizZsw&language=en&result_type=country';
+      // Link to the api
+      var link = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='+ str4.toString() +'&key=AIzaSyCy4rMsW8E0G5S7kJAH6rgXOPOl6MizZsw&language=en&result_type=country';
+      
+      // Get info from Geocoding API
+      $(document).ready(function() {
+        $.getJSON(link, function(data) {
+          // Display country name
+          document.getElementById('test').innerHTML = data.results[0].formatted_address;
+        });
+      });
+
 
       // Create a new InfoWindow.
       infoWindow = new google.maps.InfoWindow({
         position: mapsMouseEvent.latLng,
       });
       infoWindow.setContent(
-        //JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
-        'https://maps.googleapis.com/maps/api/geocode/json?latlng='+ str4.toString() +'&key=AIzaSyCy4rMsW8E0G5S7kJAH6rgXOPOl6MizZsw&language=en&result_type=country'
+        link
+        //'https://maps.googleapis.com/maps/api/geocode/json?latlng='+ str4.toString() +'&key=AIzaSyCy4rMsW8E0G5S7kJAH6rgXOPOl6MizZsw&language=en&result_type=country'
       );
       infoWindow.open(map);
     });
