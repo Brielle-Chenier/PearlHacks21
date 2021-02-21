f11c747882caa5067b1599fd33e4174d4140db55
kaylalochenier <73918913+kaylalochenier@users.noreply.github.com>
2021-02-20 19:00:06 -0500

    Create Searching_For_Artists.py

---
+++ Searching_For_Artists.py
@@ -0,0 +1,18 @@
+import csv
+
+def search(country):
+    with open('C:\\Users\\kayla\\Desktop\\Visual Studios Code\\Pearl Hacks\\artists.csv', 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+
+        local_artists = []
+
+        for row in csv_reader:
+            if row [0] == country:
+                #print (row[1])
+                local_artists.append(row[1])
+        
+        #print (local_artists)
+        add_songs_to_playlist(local_artists)
+
+
+search("United Kingdom")
