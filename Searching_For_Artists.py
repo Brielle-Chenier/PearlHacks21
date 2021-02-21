f86f3c4145a8023e10f08dc5d0f31ca1fcde9e75
Brielle-Chenier <brielle_chenier@live.com>
2021-02-21 01:37:06 -0500

    maybe some changes

--- Searching_For_Artists.py
+++ Searching_For_Artists.py
@@ -2,17 +2,18 @@ import csv
 from spotipy_app import add_songs_to_playlist
 
 def search(country):
-    with open('C:\\Users\\briel\\OneDrive\\Documents\\GitHub\\Hackathons\\PearlHacks21\\artists.csv', 'r') as csv_file:
+    with open('C:\\Users\\brie\\OneDrive\\Documents\\GitHub\\Hackathons\\PearlHacks21\\artists.csv', 'r') as csv_file:
         csv_reader = csv.reader(csv_file)
-        
+
         local_artists = []
 
         for row in csv_reader:
             if row [0] == country:
                 print (row[1])
                 local_artists.append(row[1])
-        for artist in local_artists:
-            print (artist)
-    #return add_songs_to_playlist(country, local_artists)
-search ("Canada")
-print ('hi')
+
+        print (local_artists)
+        #add_songs_to_playlist(local_artists)
+
+
+search("United Kingdom")
--- flaskApp/page1.py
+++ flaskApp/page1.py
@@ -9,11 +9,11 @@ app = Flask (__name__)
 @app.route("/<country>")
 def home(country):
     link = search(country)
-    return render_template("index.html", link = link)
+    return redirect(link)
 
 @app.route("/")
 def home1():
-    return render_template("index.html", link = "")
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
